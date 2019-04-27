78de152fa51b2f8f1144b48c242f57a09386a791
wupeng <wupeng1127@foxmail.com>
2019-04-27 15:08:20 +0800

    follow PEP8 code style

--- report_tool.py
+++ report_tool.py
@@ -7,6 +7,7 @@ import sys
 DBNAME = "news"
 USER = "vagrant"
 
+
 def connect(database_name, user):
     """
         Connect to the PostgreSQL database. Returns a database connection
@@ -26,36 +27,44 @@ def connect(database_name, user):
         print(error)
         sys.exit(1)
 
+
 def answer1():
-    """Answer the first question """
+    """Answer the first question"""
     top3_articles_sql = """
-        select a.title, t3.count 
-        from 
-            articles as a, 
+        select a.title, t3.count
+        from
+            articles as a,
             (
-                select count(*), path 
-                from log 
+                select count(*), path
+                from log
                 where path != '/'
                 and status != '404 NOT FOUND'
-                group by path 
-                order by 1 desc 
+                group by path
+                order by 1 desc
                 limit 3
-            ) as t3 
-        where concat('/article/', a.slug) = t3.path 
+            ) as t3
+        where concat('/article/', a.slug) = t3.path
         order by 2 desc;"""
-    print("Question 1: What are the most popular three articles of all time?\n")
+    print(
+        "Question 1: What are the most popular three articles of all time?\n"
+    )
     db, cursor = connect(DBNAME, USER)
     cursor.execute(top3_articles_sql)
     top3_articles = cursor.fetchall()
     cursor.close()
     db.close()
-    top3_articles_formated = "\n".join(['   "{}" - {} views'.format(x[0],x[1]) for x in top3_articles])
-    print(top3_articles_formated)
+    print(
+        "\n".join(
+            ['   "{}" - {} views'.format(x[0], x[1]) for x in top3_articles]
+        )
+    )
 
 
 def answer2():
     """Answer the second question """
-    print("Question 2: Who are the most popular article authors of all time?\n")
+    print(
+        "Question 2: Who are the most popular article authors of all time?\n"
+    )
     db, cursor = connect(DBNAME, USER)
     top_authors_sql = """
         select au.name, count(*)
@@ -68,62 +77,74 @@ def answer2():
     top_authors = cursor.fetchall()
     cursor.close()
     db.close()
-    top_authors_formated = "\n".join(['   {} - {} views'.format(x[0],x[1]) for x in top_authors])
-    print(top_authors_formated)
+    print(
+        "\n".join(
+            ['   {} - {} views'.format(x[0], x[1]) for x in top_authors]
+        )
+    )
+
 
 def answer3():
     """Answer the third question """
     error_date_sql = """
-        select * 
-        from 
+        select *
+        from
             (
-                select sc.count::float/tc.count as error_rate, sc.date 
-                from 
+                select sc.count::float/tc.count as error_rate, sc.date
+                from
                     (
-                        select total.date, count(*) 
-                        from 
+                        select total.date, count(*)
+                        from
                             (
-                                select to_char(time, 'Mon DD, YYYY') as date 
+                                select to_char(time, 'Mon DD, YYYY') as date
                                 from log
-                            ) total 
+                            ) total
                         group by 1
-                    ) tc, 
+                    ) tc,
                     (
-                        select success.date, count(*) 
-                        from 
+                        select success.date, count(*)
+                        from
                             (
-                                select to_char(time, 'Mon DD, YYYY') as date, status 
-                                from log 
+                                select
+                                    to_char(time, 'Mon DD, YYYY') as date,
+                                    status
+                                from log
                                 where status !='200 OK'
-                            ) success 
-                        group by 1 
-                    ) sc 
+                            ) success
+                        group by 1
+                    ) sc
                 where sc.date = tc.date
-            ) e 
+            ) e
         where e.error_rate > 0.01;"""
-    print("Question 3: On which days did more than 1% of requests lead to errors?\n")    
+    print(
+        "Question 3:\
+ On which days did more than 1% of requests lead to errors?\n"
+    )
     db, cursor = connect(DBNAME, USER)
     cursor.execute(error_date_sql)
     error_date = cursor.fetchall()
     cursor.close()
     db.close()
-    error_date_fromated = '   {} - {}% error'.format(
-        error_date[0][1],
-        str(round(error_date[0][0]*100, 2))
+    print(
+        '   {} - {}% error'.format(
+            error_date[0][1],
+            str(round(error_date[0][0]*100, 2))
         )
-    print(error_date_fromated)
+    )
+
 
 def run():
     """Running report ..."""
     print("Running reporting tools...\n")
     answer1()
     print("\n")
-    
+
     answer2()
     print("\n")
-    
+
     answer3()
     print("\n")
+
 
 if __name__ == '__main__':
     run()
